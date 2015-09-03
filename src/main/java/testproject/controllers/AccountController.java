c94c0fea8dc5667d1c50a204958600ff7e63774e
Jack Pham <jak464@unh.edu>
2015-09-02 22:27:39 -0400

    added post

--- src/main/java/testproject/controllers/AccountController.java
+++ src/main/java/testproject/controllers/AccountController.java
@@ -1,24 +1,41 @@
 package testproject.controllers;
 
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import testproject.cargos.Account;
 
 @Controller
 public class AccountController {
+	
+	ArrayList<Account> accountList = new ArrayList<Account>();
 
 	@RequestMapping(path = "/accounts/{accountId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	private Account getAccount(@PathVariable int accountId) {
-		Account jackAccount = new Account();
-		jackAccount.setAccountId(accountId);
-		jackAccount.setFirstName("Jack");
-		jackAccount.setLastName("Pham");
-		return jackAccount;
+	public Account getAccount(@PathVariable int accountId) {
+		for(int i = 0; i < accountList.size(); i++) {
+			if(accountList.get(i).getAccountId() == accountId ) {
+				return accountList.get(i);
+			}
+		}
+		return null;	
 	}
-
+	
+	
+	@RequestMapping(path = "/accounts", consumes = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void addAccount(@RequestBody Account account) {
+		accountList.add(account);
+	}
+	
+	
 }
